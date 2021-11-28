96144f5211e5852e43a9b68eec9d32d45756cef1
Dimas Oktanugraha <d.oktanugraha@gmail.com>
2021-11-28 21:41:26 +0700

    change json_reader

--- movie/test/json_reader.dart
+++ movie/test/json_reader.dart
@@ -6,5 +6,8 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
+    if (dir.endsWith('/movie') || dir.endsWith('\movie')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
   return File('$dir/movie/test/$name').readAsStringSync();
 }

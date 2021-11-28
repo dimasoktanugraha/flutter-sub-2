47ef343904bccc9b0b32fd2eb6c902c55c8545b4
Dimas Oktanugraha <d.oktanugraha@gmail.com>
2021-11-28 16:58:57 +0700

    test path

--- movie/test/json_reader.dart
+++ movie/test/json_reader.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 
 String readJson(String name) {
-  var dir = Directory.current.path;
+  // var dir = Directory.current.path;
+  var dir = Directory.current.parent.path;
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  return File('$dir/movie/test/$name').readAsStringSync();
 }
--- tv/test/json_reader.dart
+++ tv/test/json_reader.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 String readJson(String name) {
-  var dir = Directory.current.path;
+  var dir = Directory.current.parent.path;
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  return File('$dir/tv/test/$name').readAsStringSync();
 }
